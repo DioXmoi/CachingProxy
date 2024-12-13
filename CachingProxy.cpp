05a3f361b8cef78083184b16931a216377d4d21a
Alex Yagni <near1tfeex@gmail.com>
2024-12-13 23:20:21 +0500

    Added .gitignore file

---
+++ CachingProxy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+#include "CachingProxyConfig.h"
+
+int main() {
+
+    // report version
+    std::cout << "Version " << CachingProxy_VERSION_MAJOR << "." << CachingProxy_VERSION_MINOR << "\n";
+
+
+	std::cout << "Start Caching proxy\n";
+	return 0;
+}
